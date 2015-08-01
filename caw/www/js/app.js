0055709eb22dc48d238add156f9383bc1c2e715e
Phil Manijak <github@exclsr.com>
2015-08-01 16:55:53 -0700

    Refactor: Remove comments from the initial setup

--- caw/www/js/app.js
+++ caw/www/js/app.js
@@ -39,17 +39,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   })
 
   // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
   .state('tab.list', {
       url: '/list',
       views: {
