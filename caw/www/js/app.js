b31494bedb350d6d152e8f4e0e476fa4d0f73412
Phil Manijak <github@exclsr.com>
2015-08-01 23:42:16 -0700

    Make 'list' the default state
    Show the destination list when the app starts.

--- caw/www/js/app.js
+++ caw/www/js/app.js
@@ -90,6 +90,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/about');
+  $urlRouterProvider.otherwise('/tab/list');
 
 });
